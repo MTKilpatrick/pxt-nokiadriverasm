c1d08bf0bcb01b530c5a39f79504ee64206a57da
Michael Killpatrick <63163207+MTKilpatrick@users.noreply.github.com>
2020-05-19 23:17:15 +0100

    Update sendNokiaBuffer.asm

--- sendNokiaBuffer.asm
+++ sendNokiaBuffer.asm
@@ -28,15 +28,7 @@ sendNokiaBufferAsm:
     mov r0, r6
     bl pins::getPinAddress
     ldr r0, [r0, #8] ; get mbed DigitalOut from MicroBitPin
-    ldr r1, [r0, #4] ; r1-mask for this pin
-    ldr r2, [r0, #16] ; r2-clraddr
-    ldr r3, [r0, #12] ; r3-setaddr
-    str r1, [r2, #0] ; set data pin low
-    mov r6, r1   ; r6 is the mask data pin
-    mov r8, r2   ;  r8 is for set low data pin
-    mov r9, r3  ;  r9 is for set high data pin
-
-	
+    ldr r6, [r0, #4] ; r1-mask for this pin    
 
     ; load clock pin address into r0 and get addrs/mask
     mov r0, r7
@@ -45,45 +37,48 @@ sendNokiaBufferAsm:
     ldr r1, [r0, #4] ; r1-mask for this pin
     ldr r2, [r0, #16] ; r2-clraddr
     ldr r3, [r0, #12] ; r3-setaddr
-    str r1, [r2, #0] ; set clock pin low  (unnecessary)
-    mov r11, r2   ; r11 is for set low clock pin
-    mov r12, r3   ;  r12 is for set high clock pin
 
-    ; STATUS:
-    ; r0    loaded with buffer data
+    ; REGISTERS:
+    ; r0    to bloaded with buffer data
     ; r1    mask byte for clock pin
-    ; r2    address for set clock low
+    ; r2    address for set  low
     ; r3    used for temp storage of clr/set addr for pins
     ; r4    buffer address
     ; r5    counter
     ; r6    mask byte for data pin
     ; r7    bitmask
 
-.start:
+; r2 is low
+; r3 is high
+.start2:
     movs r7, #0x80      ; reset mask 
     ldrb r0, [r4, #0]  ; r0 := *r4               ; C2
-.common:
+.common2:
     str r1, [r2, #0]    ; clock pin := lo     ; C1
-    tst r7, r0     
-    bne .nextone                                    ; C1 + p
-	mov r3, r8      ; get the clraddr for data pin  ; C1
-.nextone:
-    str r6, [r3, #0]    ; set data pin                  ; C2
     nop
-    mov r3, r12     ; get the setaddr for clock     ; C1 
-    str r1, [r3, #0]    ; clock pin := high             ; C2
+    nop
+    tst r7, r0     
+    bne .dohigh     ; r3 is already high set so move on...  ; C1 + p
+.dolow:
+    str r6, [r2, #0]
+    b .setclockhigh
+.dohigh:
+    str r6, [r3, #0]    ; set data pin                  ; C2
+.setclockhigh:
+    nop
     nop
     lsrs r7, r7, #1     ; r6 >>= 1   C7             ; C1
-    bne .common       ;            C8             ; C1 + p
+    str r1, [r3, #0]    ; clock pin := high             ; C2
+    bne .common2       ;            C8             ; C1 + p
     ; not just a bit - need new byte
     adds r4, #1         ; r4++       C9
     subs r5, #1         ; r5--       C10
-    bne .start           ; if (r5>=0) goto .start  C11
-
+    bne .start2          ; if (r5>=0) goto .start  C11
 .stop:
-    mov r3, r11   ; prepare r3 for clock low      ; C1 
     str r1, [r2, #0]    ; clock pin := lo
 
     pop {r4,r5,r6,r7,pc}
+
+
 
 
