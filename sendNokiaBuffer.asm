54ec4584ed5281b6294a531b3c0d3a4d4fd67196
Michael Killpatrick <63163207+MTKilpatrick@users.noreply.github.com>
2020-05-20 00:07:35 +0100

    Update sendNokiaBuffer.asm

--- sendNokiaBuffer.asm
+++ sendNokiaBuffer.asm
@@ -15,15 +15,6 @@ sendNokiaBufferAsm:
     bl BufferMethods::getBytes
     mov r4, r0          ; r4 points to the data
     
-    ; setup 1st pin as digital (data)
- ;   mov r0, r6
- ;   movs r1, #0
- ;   bl pins::digitalWritePin
-    ; setup 2nd pin as digital (clock)
- ;   mov r0, r7
- ;   movs r1, #0
- ;   bl pins::digitalWritePin
-
     ; load pin address
     mov r0, r6
     bl pins::getPinAddress
@@ -38,9 +29,7 @@ sendNokiaBufferAsm:
     ldr r2, [r0, #16] ; r2-clraddr
     ldr r3, [r0, #12] ; r3-setaddr
 
-    b. start
 
-    ; REGISTERS:
     ; r0    to bloaded with buffer data
     ; r1    mask byte for clock pin
     ; r2    address for set pins low
@@ -50,26 +39,20 @@ sendNokiaBufferAsm:
     ; r6    mask byte for data pin
     ; r7    bitmask for bit testing
 
-; r2 is low
-; r3 is high
-
-
-
+    b .start
 .dohigh:                                    ; C6
     str r6, [r3, #0]    ; set data pin  hi  ; C8
     lsrs r7, r7, #1     ; r6 >>= 1          ; C9
     str r1, [r3, #0]    ; clock -> high     ; C11    
     beq .nextbyte                           ; C12           
- .common:                                   ; C0
+.common:                                    ; C0
     tst r7, r0                              ; C1
     str r1, [r2, #0]    ; clock pin := lo   ; C3
     bne .dohigh  ; r3 is high set so...     ; C4
-.dolow
     str r6, [r2, #0]  ; set data pin low    ; C6
     lsrs r7, r7, #1     ; r6 >>= 1          ; C7
     str r1, [r3, #0]    ; clock -> high     ; C9
-    bne .common                             ; C12           
-    ; not just a bit - need new byte
+    bne .common                             ; C12   
 .nextbyte:
     adds r4, #1         ; r4++       C9
     subs r5, #1         ; r5--       C10
@@ -77,9 +60,7 @@ sendNokiaBufferAsm:
 .start:                                     ; C0
     movs r7, #0x80      ; reset mask        ; C1
     ldrb r0, [r4, #0]  ; r0 := *r4          ; C3
-    b .common
-    
-
+    b .common    
 .stop:
     str r1, [r2, #0]    ; clock pin := lo
 
