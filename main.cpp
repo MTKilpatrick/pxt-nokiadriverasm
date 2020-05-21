979113c1a48d79c020d718cd1d93eeb6a7711b3a
Michael Killpatrick <63163207+MTKilpatrick@users.noreply.github.com>
2020-05-21 11:38:37 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "pxt.h"
+
+namespace nokiadriverasm {
+
+    //% shim=sendSPIBufferAsm
+    void sendSPIBufferAsm(Buffer b, int p1, int p2) {
+    }
+
+    //% shim=sendSPIByteAsm
+    void sendSPIByteAsm(int b, int p1, int p2) {
+    }
+}
